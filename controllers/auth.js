a99c90f92889c1e87f7f2328610eec59a57f4e30
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-20 22:03:06 +0900

    add route

---
+++ controllers/auth.js
@@ -0,0 +1,15 @@
+export const register = (req, res, next) => {
+  res.send('register Route')
+}
+
+export const login = (req, res, next) => {
+  res.send('login Route')
+}
+
+export const forgotpassword = (req, res, next) => {
+  res.send('forgotpassword Route')
+}
+
+export const resetPassword = (req, res, next) => {
+  res.send('resetPassword Route')
+}
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import { PORT } from './config'
+import router from './routes/auth'
 
 const app = express()
+
+app.use(express.json())
+
+app.use('/api/auth', router)
 
 const startApp = () => {
   app.listen(PORT, () => console.info(`success server port:${PORT}`))
