8d796f5643466b583e14c30e552044102129ce1e
田原 一樹 <ssp0727.lnc@gmail.com>
2021-08-21 12:48:05 +0900

    setting models

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,24 @@
-export const register = (req, res, next) => {
-  res.send('register Route')
+import User from '../models/User'
+
+export const register = async (req, res, next) => {
+  const { username, email, password } = req.body
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password
+    })
+    res.status(201).json({
+      success: true,
+      user
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message
+    })
+  }
 }
 
 export const login = (req, res, next) => {
